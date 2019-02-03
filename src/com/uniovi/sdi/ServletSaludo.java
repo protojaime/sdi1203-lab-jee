a5398441dfc9fe56ab59fe5e5217b4c7d5b98d48
protojaime <33036535+protojaime@users.noreply.github.com>
2019-02-03 19:32:05 +0100

    SDI - 2.1 Los Servlets son multihilo

--- src/com/uniovi/sdi/ServletSaludo.java
+++ src/com/uniovi/sdi/ServletSaludo.java
@@ -13,32 +13,37 @@ import javax.servlet.http.*;
 @WebServlet("/ServletSaludo")
 public class ServletSaludo extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+    int contador = 0; 
 
     /**
      * Default constructor. 
      */
-    public ServletSaludo() {
-        // TODO Auto-generated constructor stub
-    }
-    
-	/**
+   
+    public ServletSaludo() { 
+    super(); 
+    } 
+    /**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-    		throws ServletException, IOException { 
-    		// TODO Auto-generated method stub 
-    		//response.getWriter().append("Served at: ").append(request.getContextPath()); 
-    		response.setContentType("text/html"); 
-    		PrintWriter out = response.getWriter(); 
-    		out.println("<HTML>"); 
-    		out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>"); 
-    		out.println("<BODY>");
-    		String nombre = (String) request.getParameter("nombre"); if (nombre != null) { 
-    			out.println("Hola " + nombre + "<br>"); 
-    			} 
-    			out.println("</BODY></HTML>");
-}
+    protected void doGet(HttpServletRequest request, 
+    HttpServletResponse response) throws ServletException, IOException { 
+    response.setContentType("text/html"); 
+    PrintWriter out = response.getWriter(); 
+    out.println("<HTML>"); 
+    out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>"); 
+    out.println("<BODY>"); 
+    String nombre = (String) request.getParameter("nombre"); 
+    if (nombre != null) { 
+    out.println("Hola " + nombre + "<br>"); 
+    } 
+    try { 
+    Thread.sleep(15000); 
+    } catch (InterruptedException e) {} 
+    out.println("ID del hilo:"+Thread.currentThread().getId()+"<br>"); 
+    contador++; 
+    out.println("Visitas:"+contador+"<br>"); 
+    out.println("</BODY></HTML>"); 
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { 
     	doGet(request,response); 
     	}
