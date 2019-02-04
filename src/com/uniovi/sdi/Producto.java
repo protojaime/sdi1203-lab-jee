ae427ad47e49bbdc49b36d5c5bca8b74073c2d58
protojaime <33036535+protojaime@users.noreply.github.com>
2019-02-04 12:49:35 +0100

    SDI - 3.4 JSP Beans, JavaBeans y ámbitos

--- src/com/uniovi/sdi/Producto.java
+++ src/com/uniovi/sdi/Producto.java
@@ -4,7 +4,9 @@ public class Producto {
 	private String nombre;
 	private String imagen;
 	private float precio;
-
+	public Producto(){ 
+		
+	}
 	public Producto(String nombre, String imagen, float precio) {
 		this.nombre = nombre;
 		this.imagen = imagen;
